621eb7e318da431e098c0fe30707fce0b4bf2df4
Alyssa Bull <alyssabull@gmail.com>
2020-09-20 21:45:27 -0600

    Display slap message when either play slaps central pile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 var newGame = new Game;
 var centralPile = document.querySelector('.central-pile');
+var displayMessage = document.querySelector('.display-message')
 
 newGame.shuffleDeck(newGame.cards);
 newGame.beginGame();
@@ -10,23 +11,56 @@ window.addEventListener('keydown', function(event) {
   } else if (event.key === 'p') {
     player2Deals();
   } else if (event.key === 'f') {
-    newGame.slapThePile(newGame.player1);
+    player1Slaps();
   } else if (event.key === 'j') {
-    newGame.slapThePile(newGame.player2);
+    player2Slaps();
   }
 });
 
 function player1Deals() {
-  centralPile.innerHTML = "";
+  centralPile.innerHTML = '';
   newGame.playGame(newGame.player1);
   var player1Card =
   `<div><img src=${newGame.centralPile[0].src} class="player1-deck"><div>`
   centralPile.insertAdjacentHTML('afterbegin', player1Card);
 }
+
 function player2Deals() {
-  centralPile.innerHTML = "";
+  centralPile.innerHTML = '';
   newGame.playGame(newGame.player2);
   var player1Card =
   `<div><img src=${newGame.centralPile[0].src} class="player2-deck"><div>`
   centralPile.insertAdjacentHTML('afterbegin', player1Card);
+}
+
+function player1Slaps() {
+  displayMessage.innerText = '';
+  centralPile.innerHTML = ''
+  var currentCard = newGame.centralPile[0].value;
+  newGame.slapThePile(newGame.player1);
+  if (currentCard === 11) {
+    displayMessage.innerText = 'SLAPJACK! Player 1 takes the pile!';
+  } else if (currentCard === newGame.centralPile[1]) {
+    displayMessage.innerText = 'DOUBLES! Player 1 takes the pile!';
+  } else if (currentCard === newGame.centralPile[2]) {
+    displayMessage.innerText = 'SANDWICH! Player 1 takes the pile!';
+  } else {
+    displayMessage.innerText = 'BAD SLAP! Player 2 takes your top card!';
+  }
+}
+
+function player2Slaps() {
+  displayMessage.innerText = '';
+  centralPile.innerHTML = ''
+  var currentCard = newGame.centralPile[0].value;
+  newGame.slapThePile(newGame.player2);
+  if (currentCard === 11) {
+    displayMessage.innerText = 'SLAPJACK! Player 2 takes the pile!';
+  } else if (currentCard === newGame.centralPile[1]) {
+    displayMessage.innerText = 'DOUBLES! Player 2 takes the pile!';
+  } else if (currentCard === newGame.centralPile[2]) {
+    displayMessage.innerText = 'SANDWICH! Player 2 takes the pile!';
+  } else {
+    displayMessage.innerText = 'BAD SLAP! Player 1 takes your top card!';
+  }
 }
